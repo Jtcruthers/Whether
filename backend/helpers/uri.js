91fcfb07c9dee763d30053b5472a0eeb55254e4d
Justin Carruthers <jtcruthers@gmail.com>
2017-09-22 18:47:08 -0400

    Simplified helpers/uri.js

--- backend/helpers/uri.js
+++ backend/helpers/uri.js
@@ -1,9 +1,5 @@
 
-// Returns a different encodedURI location to be used as a 
-// test location depending if origin is true or false
-exports.getTestLocation = function(origin) {
-  if(origin) 
-    return encodeURI("Ohio Stadium Columbus OH");
-  return encodeURI("St Louis MO");
-}
+exports.mockOrigin = encodeURI("Ohio Stadium Columbus OH");
+
+exports.mockDestination = encodeURI("St Louis MO");
 
