90fcfa5aa79ef3678304b725361f88777a357b9f
Justin Carruthers <jtcruthers@gmail.com>
2017-10-03 03:52:46 -0400

    Added fetch actions (again)

---
+++ frontend/src/reducers/apifetch.js
@@ -0,0 +1,12 @@
+const apifetch = (state = {}, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return state;
+    case 'FETCH_SUCCESS':
+      return {...state, posts: action.payload};
+    default:
+      return state;
+  }
+}
+
+export default apifetch;
